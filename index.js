7c049f7327ba15c58e7921767d744b2f16f10f30
Nirob Hasan <nirob427417@gmail.com>
2023-03-17 11:18:38 +0600

    post

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ require('dotenv').config();
 const { Configuration, OpenAIApi } = require("openai");
 const configuration = new Configuration({
     organization: "org-UxDTw0WZ6LhokPzAH8glBPkt",
-    apiKey: process.env.OPENAI_API_KEY,
+    apiKey: "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",
 
 });
 
@@ -22,13 +22,16 @@ app.get('/', async (req, res) => {
     res.send('open Ai server is running');
 });
 app.post('/', async (req, res) => {
+    const { message } = req.body;
     const completion = await openai.createChatCompletion({
         model: "gpt-3.5-turbo",
-        messages: [{ role: "user", content: "Hello world" }],
+        prompt: `${message}`,
+        max_tokens: 100,
+        temperature: 0.5,
     });
     //console.log(completion.data.choices[0].message);
     res.json({
-        data: completion.data
+        message: completion.data.choices[0].text,
     })
 });
 
